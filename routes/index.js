cdd004e3a67e501f525823d258f6c04cfd6d879e
arowshan <arash.rowshan@gmail.com>
2022-12-20 17:28:03 -0800

    Integrate with postgres.

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,65 @@ var cors = require('cors')
 var app = express()
 app.use(cors())
 
-const todos = [];
+const pgp = require('pg-promise')(/* options */)
+
+// IMPORTANT
+// This is only for the tutorial purposes
+// Remember to NOT check in your username and password in a git repo
+// In an actual production repo you authenticate to a database differently to avoid sharing credentials.
+const db = pgp('postgres://test:testpass@localhost:5432/postgres')
+
+
+function fetchAllTodos(req, res, next) {
+  return db.any('SELECT * FROM myapp.todos')
+  .then((data) => {
+    res.json({ todos: data });
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+}
 
 app.get('/todo', function(req, res, next) {
-  res.json({ todos: todos });
+  return fetchAllTodos(req, res, next)
 });
 
 app.post('/todo', function(req, res, next) {
+
   const newTodo = req.body['todo']
-  todos.push(newTodo)
-  res.json({ todos: todos });
+
+  db.any("INSERT INTO myapp.todos(description) VALUES($1)", [newTodo])
+  .then((data) => {
+    return fetchAllTodos(req, res, next)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
 });
 
 app.delete('/todo', function(req, res, next) {
   const index = req.body['todoIndex']
-  todos.splice(index, 1);
-  res.json({ todos: todos });
+
+  db.any("DELETE FROM myapp.todos WHERE id = $1", [index])
+  .then((data) => {
+    return fetchAllTodos(req, res, next)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
 });
 
 app.put('/todo', function(req, res, next) {
   const index = req.body['todoIndex']
   const newDesc = req.body['newDesc']
-  todos[index] = newDesc;
-  res.json({ todos: todos });
+
+  db.any("UPDATE myapp.todos SET description = $1 WHERE id = $2", [newDesc, index])
+  .then((data) => {
+    return fetchAllTodos(req, res, next)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
 });
 
 module.exports = app;
